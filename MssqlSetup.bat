66a3db66f50634de48e1d7794a75356ceae10028
local-pc1 <treasureship.xxx@gmail.com>
2026-09-05 20:53:46 +0900

    Win260905

--- MssqlSetup.bat
+++ MssqlSetup.bat
@@ -1,8 +1,10 @@
 @echo off
+rem UTF-8に切り替える文字コード
+chcp 65001 >nul
 rem バッチファイルでsqlcmdとクエリファイルを操作しデータベースを編集する。
 
 :DB_CREATE
-sqlcmd -S (local)\SQLEXPRESS -i ./MssqlDbCreate.sql -v Pver="2025"
+sqlcmd -S (local)\SQLEXPRESS -i MssqlDbCreate.sql -v Pver="2022"
 
 @echo 終わりです。
 endlocal
