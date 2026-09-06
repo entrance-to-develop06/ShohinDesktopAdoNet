607c59439d183dbb94c156a6c54ddb863ca9c731
local-pc2 <treasureship.xxx@gmail.com>
2026-09-06 11:41:28 +0900

    Win260906

---
+++ MssqlTableCreate.sql
@@ -0,0 +1,26 @@
+USE [master]
+
+DECLARE @DbName varchar(30) = 'AdoNetSample1'
+DECLARE @Schema varchar(30) = 'dbo'
+DECLARE @Table varchar(30) = 'shohins'
+
+IF OBJECT_ID('[' + @DBName + '].[' + @Schema + '].[' + @Table + ']') IS NOT NULL
+	EXECUTE('DROP TABLE [' + @DBName + '].[' + @Schema + '].[' + @Table + ']')
+
+BEGIN TRY
+	BEGIN TRANSACTION
+	EXECUTE ('CREATE TABLE [' + @DBName + '].[' + @Schema + '].[' + @Table + '] (
+		unique_id char(36) NOT NULL,		/*ユニークID*/
+		shohin_code int NOT NULL,			/*商品番号*/
+		shohin_name char(50),				/*商品名*/
+		updated_on decimal(8,0) NOT NULL,   /*編集日付*/
+		updated_at decimal(6,0) NOT NULL,   /*編集時刻*/
+		remarks varchar(255),               /*備考*/
+	primary key (unique_id))')
+	COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+	PRINT (@Table + 'テーブルの作成に失敗しました。')
+	PRINT (CAST(ERROR_MESSAGE() as varchar(200)))
+END CATCH
---
+++ MssqlTableDrop.sql
@@ -0,0 +1,17 @@
+USE [master]
+
+DECLARE @DbName varchar(30) = 'AdoNetSample1'
+DECLARE @Schema varchar(30) = 'dbo'
+DECLARE @Table varchar(30) = 'shohins'
+
+BEGIN TRY
+	BEGIN TRANSACTION
+	IF OBJECT_ID('[' + @DBName + '].[' + @Schema + '].[' + @Table + ']') IS NOT NULL
+		EXECUTE('DROP TABLE [' + @DBName + '].[' + @Schema + '].[' + @Table + ']')
+	COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+	PRINT (@Table + 'テーブルの削除に失敗しました。')
+	PRINT (CAST(ERROR_MESSAGE() as varchar(200)))
+END CATCH
